fddcc5fa7cddf0a413b161f3ef77659976086c7c
aaksant <aaksangit@gmail.com>
2023-12-13 17:35:02 +0700

    Add append numbers functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+const lastOperation = document.querySelector('#last-operation');
+const currentOperation = document.querySelector('#current-operation');
+const clearButton = document.querySelector('#clear-button');
+const deleteButton = document.querySelector('#delete-button');
+
+const nums = document.querySelectorAll('.num');
+const divideButton = document.querySelector('#divide');
+const multiplyButton = document.querySelector('#multiply');
+const subtractButton = document.querySelector('#subtract');
+
+function appendNum(newNum) {
+  if (currentOperation.textContent === '0') {
+    currentOperation.textContent = newNum;
+  } else {
+    currentOperation.textContent += newNum;
+  }
+}
+
+nums.forEach((num) => {
+  num.addEventListener('click', () => appendNum(num.textContent));
+});
