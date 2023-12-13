52e70e8a4a2d9cf758d6b45d42b856943cd05d8b
aaksant <aaksangit@gmail.com>
2023-12-13 19:35:12 +0700

    Fix allowed more than one decimal point

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const nums = document.querySelectorAll('.num');
 const divideButton = document.querySelector('#divide');
 const multiplyButton = document.querySelector('#multiply');
 const subtractButton = document.querySelector('#subtract');
+const dotButton = document.querySelector('#dot-button');
 
 function appendNum(newNum) {
   if (currentOperation.textContent === '0') {
@@ -16,6 +17,13 @@ function appendNum(newNum) {
   }
 }
 
+function appendDot() {
+  if (currentOperation.textContent.includes('.')) return;
+  currentOperation.textContent += '.'
+}
+
 nums.forEach((num) => {
   num.addEventListener('click', () => appendNum(num.textContent));
 });
+
+dotButton.addEventListener('click', appendDot);
